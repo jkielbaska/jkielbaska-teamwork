81a5a5a8d9a510d4b0f20119aaad25b0693cd9a9
Jakub <kielbaskakuba@gmail.com>
2023-12-03 20:12:30 +0100

    Add logic and components to App, add export to queryClient in main, add global background file, delete unused icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,43 @@
+import { useState } from "react";
+import { useCharacters } from "@/hooks/useCharacters";
+import { DataTable } from "@/components/table/DataTable";
+import { columns } from "@/components/table/columns";
+import { LoadingSkeleton } from "./components/loaders/LoadingSkeleton";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const [page, setPage] = useState<number>(1);
+  const [searchName, setSearchName] = useState<string | null>(null);
+  const { data, isFetching, isError } = useCharacters({
+    page,
+    search: searchName,
+  });
+
+  const handleSearch = (name: string) => {
+    setSearchName(name);
+    setPage(1);
+  };
+
+  return (
+    <>
+      {data ? (
+        <div className="w-full flex items-center justify-center">
+          <DataTable
+            page={page}
+            firstPage={() => setPage(1)}
+            nextPage={() => setPage((prev: number) => prev + 1)}
+            previousPage={() => setPage((prev: number) => prev - 1)}
+            columns={columns}
+            handleSearch={handleSearch}
+            isFetching={isFetching}
+            isError={isError}
+            data={data}
+          />
+        </div>
+      ) : (
+        <LoadingSkeleton />
+      )}
+    </>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App.tsx";
 import "./index.css";
 
-const queryClient = new QueryClient();
+export const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
